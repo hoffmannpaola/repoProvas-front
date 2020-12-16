c6b192fea92b7a91b3a04fcb6bef7272d9bf1c16
hoffmannpaola <hoffmannpaola@gmail.com>
2020-12-16 17:28:37 -0300

    Layout ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 import Home from "./pages/Home";
 
 
 export default function App() {
   return (
-   
+    <>
+      <GlobalStyle />
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
         </Switch>
       </Router>
-    
+    </>
   );
 }
 
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --darkBg:  #0E0B16;
+    --generalFont: 'Chau Philomene One', sans-serif;
+  
 
+  body {
+    font-family: var(--generalFont);
+    color: white;
+  }
+
+  #root {
+    min-height: 100vh;
+    width: 100%;
+    background: var(--darkBg);
+  }
+
+`;
